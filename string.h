672f2736d47b8c0834ba3157f2ebaa77659cf9c3
wuyi <22125194@zju.edu.cn>
2022-04-11 17:23:51 +0800

    my string

---
+++ string.h
@@ -0,0 +1,32 @@
+#ifndef STRING_H
+#define STRING_H
+#include <cstring>
+#include <iostream>
+
+class String {
+ private:
+  char* data_;
+  size_t size_;
+
+ public:
+  String();
+  String(const char* str);                 // 构造
+  String(const String& other);             // 拷贝构造
+  ~String();                               // 析构
+  String& operator=(const String& other);  // 拷贝赋值
+
+  String(String&& other) noexcept;             // 移动构造
+  String& operator=(String&& other) noexcept;  // 移动赋值
+  char& operator[](size_t index);                    // 重载[]操作符
+  bool operator==(const String& other);              // 重载==操作符
+  String operator+(const String& other);
+  const char* c_str() const;
+  size_t length();
+  friend std::ostream& operator<<(std::ostream& out, const String& str);
+  friend std::istream& operator>>(std::istream& in, String& str);
+ private:
+  void __init(const char* str); // 分配内存
+  void __clean();               // 清除内存
+  void __swap(String& other); // 交换 用于移动构造
+};
+#endif
---
+++ test.cpp
@@ -0,0 +1,53 @@
+//  g++ test.cpp string.cpp -std=c++11 -fno-elide-constructors
+#include <iostream>
+#include "string.h"
+#include <cstring>
+#include <vector>
+int main() {
+
+  std::cout << "copy constructor test" << std::endl;
+  String x("xxx");
+  String cop = x; // copy constructor
+
+  std::cout << "copy assignment test" << std::endl;
+  x = cop; // copy assignment
+
+  std::cout << "move assignment test" << std::endl;
+	String a("jack");
+	String b("fluy");
+  a = std::move(b); // move assignment
+  std::cout << b << std::endl;
+  std::cout << b.length() << std::endl;
+  std::cout << a << std::endl;
+
+  std::cout << "move construct test" << std::endl;
+  String c = String("hello"); // move constructor
+  std::cout << c << std::endl;
+
+  std::cout << "operator<< test" << std::endl;
+  std::cout << c << std::endl;
+
+  std::cout << "operator>> test" << std::endl;
+  String in;
+  std::cin >> in;
+  std::cout << in << std::endl;
+
+  std::cout << "operator== test" << std::endl;
+  String aa = "hello";
+  String bb = "hello";
+  String cc = "hello1";
+  std::cout << (aa == bb) << "," << (aa == cc) << std::endl;
+
+  std::cout << "operator+ test" << std::endl;
+  String ad;
+	String bd("lucy");
+	String cd = ad + bd;
+	std::cout << cd << std::endl;
+
+  std::cout << "c_str() test" << std::endl;
+	std::cout << cd.c_str() << std::endl;
+
+  std::cout << "legth() test" << std::endl;
+	std::cout << cd.length() << std::endl;
+  return 0;
+}
